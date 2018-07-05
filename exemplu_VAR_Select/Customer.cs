5e3d4dd4cd26b412faa44c0d852211ff9415fc7f
AnaArmeanu <32650626+AnaArmeanu@users.noreply.github.com>
2018-07-05 10:30:52 +0300

    Add files via upload

---
+++ exemplu_VAR_Select/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer {
+    public string Name;
+    public string Phone;
+    public string City;
+
+    public Customer(string n, string p, string c)
+    {
+        Name = string.Copy(n);
+        Phone = string.Copy(p);
+        City = string.Copy(c);
+    }
+}
